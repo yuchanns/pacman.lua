9922fd58e664f6043fb70bf78f47d6dad64eb054
Hanchin Hsieh <me@yuchanns.xyz>
2025-11-13 14:05:59 +0800

    Add playfield

--- src/main.lua
+++ src/main.lua
@@ -7,6 +7,8 @@ local SPRITES = soluna.load_sprites "assets/sprites.dl"
 
 local TICK = 1 / 60
 local MAX_DT = 0.25
+local DISPLAY_TILE_X <const> = 28
+local DISPLAY_TILE_Y <const> = 36
 
 local last_cs
 local accumulator = 0.0
@@ -17,6 +19,52 @@ local callback = {}
 ---@type Args
 local args = ...
 local BATCH = args.batch
+local BASE_WIDTH <const> = args.width
+local BASE_HEIGHT <const> = args.height
+
+local viewport = {
+    ---@type number
+    scale = 1,
+    ---@type number
+    offset_x = 0,
+    ---@type number
+    offset_y = 0,
+}
+
+---@param f fun()
+---@return fun()
+function viewport:draw(f)
+    return function()
+        BATCH:layer(self.scale, self.offset_x, self.offset_y)
+        f()
+        BATCH:layer()
+    end
+end
+
+function viewport:resize(w, h)
+    if not w or not h then
+        return
+    end
+    local border <const> = 20
+    w = w - border
+    h = h - border
+    if w <= 0 or h <= 0 then
+        viewport.scale = 1
+        viewport.offset_x = 0
+        viewport.offset_y = 0
+        return
+    end
+    local ref_w = BASE_WIDTH > 0 and BASE_WIDTH or w
+    local ref_h = BASE_HEIGHT > 0 and BASE_HEIGHT or h
+    ---@type number
+    local scale = math.min(w / ref_w, h / ref_h)
+    if scale <= 0 then
+        scale = 1
+    end
+    self.scale = scale
+    self.offset_x = (1.0 - scale) * (w * 0.5)
+    self.offset_y = (1.0 - scale) * (h * 0.5)
+end
 
 local blinky_anim8 = {
     ---@type table<number, number|userdata|string|nil>
@@ -76,11 +124,79 @@ local layout_pos = util.cache(function(k)
     return (layout.calc(doms[k]))
 end)
 
-local hud = {}
+local hud = {
+    tiles = {}
+}
+
+local function game_init_playfield()
+    local tiles <const> = {
+        "0UUUUUUUUUUUU45UUUUUUUUUUUU1",
+        "L............rl............R",
+        "L.ebbf.ebbbf.rl.ebbbf.ebbf.R",
+        "LPr  l.r   l.rl.r   l.r  lPR",
+        "L.guuh.guuuh.gh.guuuh.guuh.R",
+        "L..........................R",
+        "L.ebbf.ef.ebbbbbbf.ef.ebbf.R",
+        "L.guuh.rl.guuyxuuh.rl.guuh.R",
+        "L......rl....rl....rl......R",
+        "2BBBBf.rzbbf rl ebbwl.eBBBB3",
+        "     L.rxuuh gh guuyl.R     ",
+        "     L.rl          rl.R     ",
+        "     L.rl mjs--tjn rl.R     ",
+        "UUUUUh.gh i      q gh.gUUUUU",
+        "      .   i      q   .      ",
+        "BBBBBf.ef i      q ef.eBBBBB",
+        "     L.rl okkkkkkp rl.R     ",
+        "     L.rl          rl.R     ",
+        "     L.rl ebbbbbbf rl.R     ",
+        "0UUUUh.gh guuyxuuh gh.gUUUU1",
+        "L............rl............R",
+        "L.ebbf.ebbbf.rl.ebbbf.ebbf.R",
+        "L.guyl.guuuh.gh.guuuh.rxuh.R",
+        "LP..rl.......  .......rl..PR",
+        "6bf.rl.ef.ebbbbbbf.ef.rl.eb8",
+        "7uh.gh.rl.guuyxuuh.rl.gh.gu9",
+        "L......rl....rl....rl......R",
+        "L.ebbbbwzbbf.rl.ebbwzbbbbf.R",
+        "L.guuuuuuuuh.gh.guuuuuuuuh.R",
+        "L..........................R",
+        "2BBBBBBBBBBBBBBBBBBBBBBBBBB3",
+    }
+    for y = 1, DISPLAY_TILE_Y do
+        for x = 1, DISPLAY_TILE_X do
+            local sprite
+            if y >= 4 and y <= 34 then
+                local i = y - 3
+                local line = assert(tiles[i])
+                local c = line:sub(x, x)
+                if c == "." then
+                    sprite = SPRITES.tile_10
+                elseif c ~= "" then
+                    sprite = SPRITES["tile_" .. c]
+                end
+            end
+            table.insert(hud.tiles, {
+                sprite = sprite,
+                x = (x - 1) * 16,
+                y = (y - 1) * 16,
+            })
+        end
+    end
+end
+
+game_init_playfield()
 
 -- see @assets/layouts/hud.dl > region : map
 function hud.map(self)
-    blinky_anim8:draw(self.x, self.y)
+    BATCH:layer(self.x, self.y)
+    -- in the middle of the map, draw blinky animation
+    blinky_anim8:draw((13 * 16), (16 * 16))
+    for _idx, tile in ipairs(hud.tiles) do
+        if tile.sprite then
+            BATCH:add(tile.sprite, tile.x, tile.y)
+        end
+    end
+    BATCH:layer()
 end
 
 ---@type table<string, table> & string[]
@@ -98,10 +214,10 @@ function layouts:resize(w, h)
     end
 end
 
-layouts:resize(args.width, args.height)
+viewport:resize(BASE_WIDTH, BASE_HEIGHT)
+layouts:resize(BASE_WIDTH, BASE_HEIGHT)
 
-
-local function draw()
+local draw = viewport:draw(function()
     for i = 1, #layouts do
         local name = layouts[i]
         local pos = layout_pos[name]
@@ -114,7 +230,7 @@ local function draw()
             end
         end
     end
-end
+end)
 
 function callback.frame(_count)
     local _, now_cs = ltask.now()
@@ -138,6 +254,7 @@ function callback.frame(_count)
 end
 
 function callback.window_resize(w, h)
+    viewport:resize(w, h)
     layouts:resize(w, h)
 
     draw()
